379584fa5c3ccecd67e19bcb6ad2fffb9538bc55
vivek sahani <54721640+SilenMe@users.noreply.github.com>
2024-02-09 14:35:42 +0530

    added Accepted solution

--- 368- Largest Divisible Subset.py
+++ 368- Largest Divisible Subset.py
@@ -19,7 +19,20 @@ class Solution:
 
             d[i+1]=sol
         return d[len(d)-1][0]
-        
+#accepted solution
+# the thing I was cheking the factor from starting no from end. The benifit of starting from end is that at every step we get only the optimised list item, rather than getting the entire range of subset which we get if we start from 0 index
+class Solution: 
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]: 
+        nums.sort() 
+        n = len(nums) 
+        dp = [[val] for val in nums] 
+        res = [] 
+        for i in range(n - 1, -1, -1): 
+            for j in range(i + 1, n): 
+                if nums[j] % nums[i] == 0 and len(dp[j]) >= len(dp[i]): 
+                    dp[i] = [nums[i]] + dp[j] 
+            res = dp[i] if len(dp[i]) > len(res) else res 
+        return res
 
 
                 
