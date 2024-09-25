1ba27ee964a06e27c9f415ba3af727af2ffb1450
vivek sahani <54721640+SilenMe@users.noreply.github.com>
2024-09-25 20:31:16 +0530

    Update 2416. Sum of Prefix Scores of Strings.py

--- 2416. Sum of Prefix Scores of Strings.py
+++ 2416. Sum of Prefix Scores of Strings.py
@@ -19,3 +19,39 @@ class Solution:
                 count+=mainSet[st]
             sol.append(count)
         return sol
+
+
+#trie solution more optimized
+
+class PrefixNode:                  # to store nodes on trie
+    def __init__(self):
+        self.children=[None]*26
+        self.count=0
+class PrefixTree:                  #initiate a trie of node 
+    def __init__(self):
+        self.root=PrefixNode()
+    def insert(self,w):
+        cur=self.root
+        for c in w:
+            i=ord(c)-ord('a')
+            if not cur.children[i]:
+                cur.children[i]=PrefixNode()
+            cur=cur.children[i]
+            cur.count+=1
+    def get_score(self,w):
+        cur=self.root
+        score=0
+        for c in w:
+            i=ord(c)-ord('a')
+            cur=cur.children[i]
+            score+=cur.count
+        return score
+class Solution:
+    def sumPrefixScores(self,words:List[str])->List[int]:
+        res=[]
+        prefix_tree=PrefixTree()
+        for w in words:
+            prefix_tree.insert(w)
+        for w in words:
+            res.append(prefix_tree.get_score(w))
+        return res
