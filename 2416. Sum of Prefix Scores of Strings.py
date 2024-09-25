09bb729fe27d3e0318cf187f089a320710b65a9c
vivek sahani <54721640+SilenMe@users.noreply.github.com>
2024-09-25 20:07:43 +0530

    Update 2416. Sum of Prefix Scores of Strings.py

--- 2416. Sum of Prefix Scores of Strings.py
+++ 2416. Sum of Prefix Scores of Strings.py
@@ -1,21 +1,21 @@
 #after a really long time I'm able to solve a leetcode hard problem without a single Googling: all thanks to yersterday daily problem
 #the code is not really efficient, trying a different approach
 class Solution:
-    def sumPrefixScores(self,k:List[str])->List[int]:
-        M={}
-        q=[]
-        for y in k:
-            for i in range(1,len(y)+1):
-                if y[0:i] in M:
-                    M[y[0:i]]+=1
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+        mainSet={}
+        sol=[]
+        for item in words:
+            for i in range(1,len(item)+1):
+                if item[0:i] in mainSet:
+                    mainSet[item[0:i]]+=1
                 else:
-                    M[y[0:i]]=1
-        for y in k:
-            r=[]
-            z=0
-            for i in range(1,len(y)+1):
-                r.append(y[0:i])
-            for st in r:
-                z+=M[st]
-            q.append(z)
-        return q
+                    mainSet[item[0:i]]=1
+        for item in words:
+            temp = []
+            count=0
+            for i in range(1,len(item)+1):
+                temp.append(item[0:i])
+            for st in temp:
+                count+=mainSet[st]
+            sol.append(count)
+        return sol
