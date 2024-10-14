358b78c3b48f6e263c92141b7beba7ee5a810405
vivek sahani <54721640+SilenMe@users.noreply.github.com>
2024-10-14 17:40:45 +0530

    Create 2530. Maximal Score After Applying K Operations.py

---
+++ 2530. Maximal Score After Applying K Operations.py
@@ -0,0 +1,13 @@
+#we don't have maxHeap built in python so we will use min heap by converting the all the values in negative
+class Solution:
+    def maxKelements(self, nums: List[int], k: int) -> int:
+        ans=0
+        minHeapArr=[-n for n in nums]
+        heapq.heapify(minHeapArr)
+        while k:
+            n=-heapq.heappop(minHeapArr)
+            ans+=n
+            k-=1
+            heapq.heappush(minHeapArr,-((n//3)+1))
+        return ans
+        
