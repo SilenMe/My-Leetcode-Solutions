609138da8409c51a6192e9804d6b95cfdcb4ffd5
vivek sahani <54721640+SilenMe@users.noreply.github.com>
2024-12-11 21:02:07 +0530

    Create 2779. Maximum Beauty of an Array After Applying Operation.py

---
+++ 2779. Maximum Beauty of an Array After Applying Operation.py
@@ -0,0 +1,11 @@
+#for this problem hint is the solution xD
+class Solution:
+    def maximumBeauty(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        ans=0
+        l=0
+        for r in range(len(nums)):
+            while nums[r]-nums[l]>2*k:
+                l+=1
+            ans=max(ans,r-l+1)
+        return ans
